6e61972b8e70f8e7d2110a1582ad0f9421c14f60
Kaspars Dambis <hi@kaspars.net>
2025-05-12 15:06:39 +0300

    ID 0 is reserved for root

--- tests/tools/parser/class-wp-api-client-test.php
+++ tests/tools/parser/class-wp-api-client-test.php
@@ -9,33 +9,33 @@ class WP_API_Client_Test extends \PHPUnit\Framework\TestCase {
 
 		// Mix-up keys as IDs but expect the correct order.
 		$posts = [
-			'top0' => 7,
-			'top1' => 4,
-			'- top1-child1' => 2,
-			'-- top1-child1-child1' => 6,
-			'-- top1-child1-child2' => 1,
-			'top2' => 5,
-			'- top2-child1' => 3,
-			'- top2-child2' => 0,
+			'top0' => 8,
+			'top1' => 5,
+			'- top1-child1' => 3,
+			'-- top1-child1-child1' => 7,
+			'-- top1-child1-child2' => 2,
+			'top2' => 6,
+			'- top2-child1' => 4,
+			'- top2-child2' => 1,
 		];
 
 		$post_parents = [
-			7 => 0,
-			4 => 0,
-			2 => 4,
-			6 => 2,
-			1 => 2,
+			8 => 0,
 			5 => 0,
 			3 => 5,
-			0 => 5,
+			7 => 3,
+			2 => 3,
+			6 => 0,
+			4 => 6,
+			1 => 6,
 		];
 		
 		$post_order = [
-			7 => 0,
-			4 => 5,
-			1 => 10,
-			5 => 20,
-			3 => -1,
+			8 => 0,
+			5 => 5,
+			2 => 10,
+			6 => 20,
+			4 => -1,
 		];
 
 		$sorted_posts = $client->sort_posts( array_flip( $posts ), $post_parents, $post_order );
